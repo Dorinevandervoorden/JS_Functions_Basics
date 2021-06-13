036cdf1321547631650c09679b0cb1f32dad5a70
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-13 20:27:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hallo Winc Academy Studenten");
+
+// Make Pizza
+const makePizza = function () {
+    console.log("Make Dough");
+    console.log("Make Tomato Sauce");
+    console.log("Cut The Veggies and Meat");
+}
+
+// Make Sushi
+const makeSushi = function () {
+    console.log("Cook Rice");
+    console.log("Cut Fish");
+    console.log("Put Rice and Fish Together");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
